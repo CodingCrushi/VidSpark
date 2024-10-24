364bad4b59109204a8d7a7f22893c373011eae52
Homayoun <homayoun763@gmail.com>
2024-10-25 02:33:39 +0330

    Overview of Web and Cloud Development

--- app/(auth)/user-profile/[[...user-profile]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { UserProfile } from "@clerk/nextjs";
-
-const UserProfilePage = () => {
-  return (
-    <div className="flex justify-center"> <UserProfile path="/user-profile" /></div>
-  )
-}
-
-export default UserProfilePage
